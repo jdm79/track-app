acf60a1a110a31765c9c12c5311405912c7cb4fd
jdm79 <james.malvern@gmail.com>
2023-01-13 16:49:42 +0000

    Wire signup to signIn screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
     return (
-            <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+        <>
+                <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+                <Button 
+                    title="Go to Signin" 
+                    onPress={() => navigation.navigate('SignIn')}
+                />
+        </>
     )
 
 };
