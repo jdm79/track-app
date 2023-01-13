140c157b9e448fe73b822fa482238d5090bf03b1
jdm79 <james.malvern@gmail.com>
2023-01-13 19:09:43 +0000

    Style the container using flexbox, marginBottom to move the text closer to the top

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../../components/Spacer";
 
@@ -9,7 +9,7 @@ import Spacer from "../../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
     return (
-        <>
+        <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign up for Tracker</Text>
             </Spacer>
@@ -19,7 +19,7 @@ const SignupScreen = ({ navigation }) => {
             <Spacer>
                 <Button title="Sign up" />
             </Spacer>
-        </>
+        </View>
     );
 };
 
@@ -30,7 +30,11 @@ SignupScreen.navigationOptions = () => {
   };
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1, // this makes the container take up the whole screen
+        justifyContent: 'center',
+        marginBottom: 250
+    }
 });
 
 export default SignupScreen;
