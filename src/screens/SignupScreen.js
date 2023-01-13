5fd7b2ada3a6409d874cc2ddaf60f9bbd16ca7a8
jdm79 <james.malvern@gmail.com>
2023-01-13 19:27:58 +0000

    Hook up the state for email and password, use secureTextEntry for password

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from 'react-native';
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../../components/Spacer";
@@ -8,14 +8,31 @@ import Spacer from "../../components/Spacer";
 // Styled buttons etc
 
 const SignupScreen = ({ navigation }) => {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    
     return (
         <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign up for Tracker</Text>
             </Spacer>
-            <Input label="Email" />
+            {/* you could use onChangeText={setEmail} also */}
+            <Input 
+                label="Email" 
+                value={email} 
+                onChangeText={(newEmail) => setEmail(newEmail)}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
             <Spacer />
-            <Input label="Password" />
+            <Input 
+                label="Password" 
+                value={password} 
+                onChangeText={(newPassword) => setPassword(newPassword)} 
+                autoCapitalize="none"    
+                autoCorrect={false}
+                secureTextEntry
+            />
             <Spacer>
                 <Button title="Sign up" />
             </Spacer>
