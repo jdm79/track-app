0ddc19efbad32f7ac6b5af7b62bae3a220b2add9
jdm79 <james.malvern@gmail.com>
2023-01-27 17:09:02 +0000

    add safeareaview to make content viewable on screen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { SafeAreaView } from "react-navigation";
 import { View, StyleSheet, Text } from 'react-native';
 
 const TrackCreateScreen = () => {
     return (
+        <SafeAreaView>
             <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>
+        </SafeAreaView>
     )
 
 };
