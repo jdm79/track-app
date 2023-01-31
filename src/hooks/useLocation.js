a4ebdd73c6d4c1482fe28a4df8f3aca51c8a9a09
jdm79 <james.malvern@gmail.com>
2023-01-31 21:22:54 +0000

    refactor TrackCreateScreen, comment out lines 42-44 on useLocation - crashes app when navigating between screens

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -8,46 +8,46 @@ import {
 
 export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null);
-    const [subscriber, setSubscriber] = useState(null)
-
-    const startWatching = async () => {
-        try {
-            const { granted } = await requestForegroundPermissionsAsync();
-            const sub = await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10
-            },
-            callback,
-
-            location => {
-                addLocation(location)
-            }
-          );
-
-          if (!granted) {
-            throw new Error('Location permission not granted');
-          }
-
-          setSubscriber(sub)
-
-
-        } catch (e) {
-          setErr(e);
-        }
-      };
 
       useEffect(() => {
-        if(shouldTrack) {
+        let subscriber;
+        const startWatching = async () => {
+            try {
+                const { granted } = await requestForegroundPermissionsAsync();
+                subscriber = await watchPositionAsync({
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000,
+                    distanceInterval: 10
+                },
+                callback,
+    
+                location => {
+                    addLocation(location)
+                }
+              );
+    
+              if (!granted) {
+                throw new Error('Location permission not granted');
+              }    
+    
+            } catch (e) {
+              setErr(e);
+            }
+          };
+
+        if (shouldTrack) {
             startWatching();
         } else {
-            subscriber.remove();
-            setSubscriber(null);
+            // below causes error when navigating between screens
+            // if (subscriber) {
+            //     subscriber.remove();
+            // }
+            subscriber = null;
         }
 
         return () => {
-            if(subscriber) {
-                subscriber.remove()
+            if (subscriber) {
+                subscriber.remove();
             }
         };
     }, [shouldTrack, callback]);
